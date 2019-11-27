f83e9a431b1f3092659acacdf01386643b560d39
Andrew Birnberg <andrew.birnberg@gmail.com>
2019-11-27 14:51:18 -0800

    first 4 execises

---
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def matrix_transpose(matrix):
+    """Perform matrix transpose"""
+    nrows = len(matrix)
+    ncols = len(matrix[0])
+    transpose = [[0 for _ in range(nrows)] for _ in range(ncols)]
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            transpose[j][i] = matrix[i][j]
+    return transpose
