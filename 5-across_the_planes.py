9d6cdfedb0eeeb1e763698ce6c3f179738a4ceb0
Andrew Birnberg <andrew.birnberg@gmail.com>
2019-11-29 18:08:22 -0800

    exercises 4-8

---
+++ 5-across_the_planes.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+def add_matrices2D(mat1, mat2):
+    """Add two same dimension matrices element-wise"""
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
+        return None
+    return [[x + y for x, y in zip(mat1[i], mat2[i])] for i in range(len(mat1))]
