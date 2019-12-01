59df409ba3784b800c6d389f4b04c32b43975b07
Andrew Birnberg <andrew.birnberg@gmail.com>
2019-11-30 23:51:00 -0800

    Assume matrix is already ndarray

--- math/0x00-linear_algebra/11-the_western_exchange.py
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """Demonstration numpy transpose method"""
-import numpy as np
 
 
 def np_transpose(matrix):
     """Perform transpose of matrix"""
-    return np.array(matrix).T
+    return matrix.T
