236c579421db3095ef0daec6b7def26f82cdf630
Andrew Birnberg <andrew.birnberg@gmail.com>
2019-11-30 23:53:28 -0800

    assume input matrices are numpy arrays

--- math/0x00-linear_algebra/13-cats_got_your_tongue.py
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -5,6 +5,4 @@ import numpy as np
 
 def np_cat(mat1, mat2, axis=0):
     """Concatenate mat1 and mat2 along the specified axis"""
-    mat1 = np.array(mat1)
-    mat2 = np.array(mat2)
     return np.concatenate([mat1, mat2], axis=axis)
