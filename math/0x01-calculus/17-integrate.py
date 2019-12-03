1eb14ddb715e464815fbb117c57cdeda264538d1
Andrew Birnberg <andrew.birnberg@gmail.com>
2019-12-02 23:53:20 -0800

    ensure at least one element in return list

--- math/0x01-calculus/17-integrate.py
+++ math/0x01-calculus/17-integrate.py
@@ -7,6 +7,6 @@ def poly_integral(poly, C=0):
     if not all(type(c) in (float, int) for c in poly) or type(C) != int:
         return None
     integral = [C] + [c/i if c % i != 0 else c//i for i, c in enumerate(poly, 1)]
-    while len(integral) > 0 and integral[-1] == 0:
+    while len(integral) > 1 and integral[-1] == 0:
         integral.pop()
     return integral
