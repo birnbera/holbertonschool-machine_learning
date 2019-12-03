f0091d4d576504cb0f489cb3724ca01668a46703
Andrew Birnberg <andrew.birnberg@gmail.com>
2019-12-02 23:50:27 -0800

    ensure possibility of returning empty list

--- math/0x01-calculus/17-integrate.py
+++ math/0x01-calculus/17-integrate.py
@@ -6,6 +6,7 @@ def poly_integral(poly, C=0):
     """Integrate poly with constant factor C"""
     if not all(type(c) in (float, int) for c in poly) or type(C) != int:
         return None
-    if len(poly) == 0 and C == 0:
-        return []
-    return [C] + [c/i if c % i != 0 else c//i for i, c in enumerate(poly, 1)]
+    integral = [C] + [c/i if c % i != 0 else c//i for i, c in enumerate(poly, 1)]
+    while len(integral) > 0 and integral[-1] == 0:
+        integral.pop()
+    return integral
