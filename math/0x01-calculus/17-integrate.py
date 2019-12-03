6719a57c6117ee84121ea98ba483decea19f7306
Andrew Birnberg <andrew.birnberg@gmail.com>
2019-12-02 21:50:25 -0800

    check valid input

--- math/0x01-calculus/17-integrate.py
+++ math/0x01-calculus/17-integrate.py
@@ -4,4 +4,8 @@
 
 def poly_integral(poly, C=0):
     """Integrate poly with constant factor C"""
+    if not poly \
+       or not all(isinstance(coef, (float, int)) for coef in poly) \
+       or not isinstance(C, (float, int)):
+        return None
     return [C] + [coef/i for i, coef in enumerate(poly, 1)]
