f9d5b3cb96c45f4ec92bbab1ef372b98d0cee862
Andrew Birnberg <andrew.birnberg@gmail.com>
2019-12-16 00:14:48 -0800

    add pdf method to normal distribution; ex9

--- math/0x03-probability/normal.py
+++ math/0x03-probability/normal.py
@@ -33,3 +33,7 @@ class Normal:
     def x_value(self, z):
         """Calculate the value that x would take with a z-score of z"""
         return z*self.stddev + self.mean
+
+    def pdf(self, x):
+        """Calculate the pdf at x"""
+        return 1/(self.stddev * (2*π)**0.5) * e**(-1/2 * self.z_score(x)**2)
