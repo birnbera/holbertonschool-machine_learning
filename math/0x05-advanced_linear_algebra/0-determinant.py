4a40e5f24b3662037000f34dd5e5fc762fbb1fb1
Andrew Birnberg <andrew.birnberg@gmail.com>
2020-02-15 19:00:16 -0800

    change to one function

--- math/0x05-advanced_linear_algebra/0-determinant.py
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -18,26 +18,25 @@ def determinant(matrix):
     except AssertionError:
         raise ValueError('matrix must be a square matrix')
 
-    return _determinant(matrix)
-
-
-def _determinant(matrix):
-    """Recursive function to calculate determinant of NxN matrix"""
-    if len(matrix) == 1:
-        return matrix[0][0]
-    if len(matrix) == 2:
-        return matrix[0][0]*matrix[1][1] - matrix[0][1]*matrix[1][0]
-    else:
-        first_row = matrix[0]
-        sub_matrices = [
-            [
+    def _determinant(matrix):
+        """Recursive function to calculate determinant of NxN matrix"""
+        if len(matrix) == 1:
+            return matrix[0][0]
+        if len(matrix) == 2:
+            return matrix[0][0]*matrix[1][1] - matrix[0][1]*matrix[1][0]
+        else:
+            first_row = matrix[0]
+            sub_matrices = [
                 [
-                    row[idx2] for idx2 in range(len(row)) if idx2 != idx1
-                ] for row in matrix[1:]
-            ] for idx1 in range(len(first_row))
-        ]
-        return sum(
-            (-1)**idx * first_row_element * _determinant(sub_matrix)
-            for idx, (first_row_element, sub_matrix) in
-            enumerate(zip(first_row, sub_matrices))
-        )
+                    [
+                        row[idx2] for idx2 in range(len(row)) if idx2 != idx1
+                    ] for row in matrix[1:]
+                ] for idx1 in range(len(first_row))
+            ]
+            return sum(
+                (-1)**idx * first_row_element * _determinant(sub_matrix)
+                for idx, (first_row_element, sub_matrix) in
+                enumerate(zip(first_row, sub_matrices))
+            )
+
+    return _determinant(matrix)
